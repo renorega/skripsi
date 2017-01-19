525786d0875be0570901a4c08ba7eb7fa4a041d9
Reno Ghaffur <renoghaffur3218@gmail.com>
2017-01-19 15:54:42 +0700

    kmeans 1 done!

--- myTheses/main.cpp
+++ myTheses/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     //loading image
     Mat img,imgHSV,imgHSVFull;
     // use IMREAD_COLOR to access image in BGR format as 8 bit image
-    img = imread("/home/reno/skripsi/ALL_SAMPLES/ALL_Sardjito/gambar_29mei/AfarelAzis_17april_01680124/1-4.jpg",IMREAD_COLOR);
+    img = imread("/home/reno/skripsi/ALL_SAMPLES/ALL_Sardjito/gambar_29mei/AfarelAzis_17april_01680124/8-9.jpg",IMREAD_COLOR);
     namedWindow("Original",WINDOW_NORMAL);
     imshow("Original",img);
 
@@ -79,6 +79,100 @@ int main(int argc, char *argv[])
     namedWindow("Image Median Full2",WINDOW_NORMAL);
     imshow("Image Median Full2",imgMedianFull2);
     */
+
+    //buat vector bgr tipe Mat
+    vector<Mat> imgK,imgKFull;
+
+    split(imgMedian, imgK);
+    split(imgMedianFull, imgKFull);
+
+    namedWindow("S",WINDOW_NORMAL);
+    imshow("S", imgK[1]);
+    namedWindow("SFull",WINDOW_NORMAL);
+    imshow("SFull", imgKFull[1]);
+
+    //buat matrix P kosong dengan ukuran row = column x row, col=3 dan tipe CV_32F
+    Mat p = Mat::zeros(img.cols*img.rows, 3, CV_32F);
+    Mat p2 = Mat::zeros(img.cols*img.rows, 3, CV_32F);
+    Mat bestLabels,bestLabels2,centers,centers2, clustered,clustered2;
+
+    for(int i=0; i<img.cols*img.rows; i++) {
+        p.at<float>(i,0) = (i/img.cols) / img.rows; // what is this for ?
+        p.at<float>(i,1) = (i%img.cols) / img.cols; // what is this for ?
+        p.at<float>(i,2) = imgK[1].data[i] / 255.0; // how to use only one channel (S)
+    }
+
+    for(int i=0; i<img.cols*img.rows; i++) {
+        p2.at<float>(i,0) = (i/img.cols) / img.rows; // what is this for ?
+        p2.at<float>(i,1) = (i%img.cols) / img.cols; // what is this for ?
+        p2.at<float>(i,2) = imgKFull[1].data[i] / 255.0; // how to use only one channel (S)
+    }
+
+    int K = 4;
+    kmeans(p, K, bestLabels,
+            TermCriteria( CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 10, 1.0),
+            3, KMEANS_PP_CENTERS, centers);
+
+    kmeans(p2, K, bestLabels2,
+            TermCriteria( CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 10, 1.0),
+            3, KMEANS_PP_CENTERS, centers2);
+
+    //buat ngasih warna
+    int colors[K]; //0 adalah hitam, 255 adalah putih
+    colors[0] = 255;
+    colors[1] = 0;
+    colors[2] = 255;
+    colors[3] = 255;
+
+    // Ngereshape plus ngasih warna dengan best labels
+    clustered = Mat(img.rows, img.cols, CV_32F);
+    for(int i=0; i<img.cols*img.rows; i++) {
+        clustered.at<float>(i/img.cols, i%img.cols) = (float)(colors[bestLabels.at<int>(0,i)]);
+    }
+
+    clustered2 = Mat(img.rows, img.cols, CV_32F);
+    for(int i=0; i<img.cols*img.rows; i++) {
+        clustered2.at<float>(i/img.cols, i%img.cols) = (float)(colors[bestLabels2.at<int>(0,i)]);
+    }
+
+    Mat imgKFinal(960,1280,CV_8UC3);
+    Mat imgKFinalFull(960,1280,CV_8UC3);
+    for(int y=0;y<img.rows;y++){
+        for(int x=0;x<img.cols;x++){
+            if(clustered.at<float>(y,x)==0)
+            {
+                imgKFinal.at<Vec3b>(y,x)= img.at<Vec3b>(y,x);
+            }
+
+           else
+            {
+                imgKFinal.at<Vec3b>(y,x) = {255,255,255};
+            }
+        }
+    }
+
+    for(int y=0;y<img.rows;y++){
+        for(int x=0;x<img.cols;x++){
+            if(clustered2.at<float>(y,x)==0)
+            {
+                imgKFinalFull.at<Vec3b>(y,x)= img.at<Vec3b>(y,x);
+            }
+
+           else
+            {
+                imgKFinalFull.at<Vec3b>(y,x) = {255,255,255};
+            }
+        }
+    }
+
+    //clustered.convertTo(clustered, CV_8U);
+    //clustered2.convertTo(clustered2, CV_8U);
+
+    namedWindow("clustered",WINDOW_NORMAL);
+    imshow("clustered", imgKFinal);
+    namedWindow("clusteredFull",WINDOW_NORMAL);
+    imshow("clusteredFull", imgKFinalFull);
+
     waitKey();
     destroyAllWindows();
     return 0;
